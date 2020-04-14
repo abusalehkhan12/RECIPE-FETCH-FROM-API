61d96e6402a63d67d0c833804dba402ac612fcb4
abusalehkhan12 <abusaleh.khan12@gmail.com>
2020-04-14 21:23:56 +0530

    Updates

---
+++ precache-manifest.b2517ca246f1dd2de9152fc89f958048.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f18e50772916b962ea723e6035a4ed",
+    "url": "/RECIPE-FETCH-FROM-API/index.html"
+  },
+  {
+    "revision": "d188d4fd110b7cf3a746",
+    "url": "/RECIPE-FETCH-FROM-API/static/css/main.c1c2ac32.chunk.css"
+  },
+  {
+    "revision": "8be97da0692cce53933a",
+    "url": "/RECIPE-FETCH-FROM-API/static/js/2.5a57aa99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RECIPE-FETCH-FROM-API/static/js/2.5a57aa99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d188d4fd110b7cf3a746",
+    "url": "/RECIPE-FETCH-FROM-API/static/js/main.2745fad7.chunk.js"
+  },
+  {
+    "revision": "84f2778110c21bd452c6",
+    "url": "/RECIPE-FETCH-FROM-API/static/js/runtime-main.c90a2352.js"
+  }
+]);
